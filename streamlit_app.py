6d85592e1df07e52da9316a58835d4b28674e7ac
electric-otter <krw122081@gmail.com>
2024-12-15 06:00:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("NightAI")
+st.write('NightAI is a AI lab who wants to make AI better by open source AI creation')
